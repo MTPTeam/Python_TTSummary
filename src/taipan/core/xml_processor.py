fix stabling count double scaling bug for units
- where 0.5 would be scaled to add 0.25 to the count. ensure all unit calculations are normalised to 6 car trains - fail if condition is not met

src/taipan/core/xml_processor.py
@@ -209,7 +209,6 @@ def merge_out_in_per_day(out_list, in_list, sort_by_unit=True):
     return merged
 
 
-
 def startofdayunitcount(daylist, u_list):
     """ 
     Finds the minimum number of units stabled at each location at the start of the day
@@ -250,113 +249,84 @@ def startofdayunitcount(daylist, u_list):
     return [total_required] + per_unit_aligned
 
 
-
 def endofdayunitcount(daylist, u_list, change_matrix):
-    """ 
-    Finds the end of day balance between units at the start of the day and units at the end of the day
-    An output of zero means the stabling location is balanced for that day
-    """
-    
-    startcount = startofdayunitcount(daylist, u_list)
-    stablechange = np.array(startcount)
-    
-    
-    for entry in daylist:
-        threecarscalar = 0.5 if entry[3] == 3 else 1
-
-        if entry[8] < 0:
-            stablechange -= np.array(change_matrix.get(entry[2]))*threecarscalar
-            
-        else:
-            stablechange += np.array(change_matrix.get(entry[2]))*threecarscalar
-            
-            
-    total = stablechange[0]-startcount[0]
-    breakdown  = list(stablechange[1:]-np.array(startcount[1:]))
-    
-    return total,breakdown
+   """
+   Finds the end of day balance between units at the start of the day and units at the end of the day
+   An output of zero means the stabling location is balanced for that day
+   """
+   startcount = startofdayunitcount(daylist, u_list)
+   stablechange = np.array(startcount)
+   for entry in daylist:
+       if entry[8] < 0:
+           stablechange -= np.array(change_matrix.get(entry[2])) * abs(entry[8])
+       else:
+           stablechange += np.array(change_matrix.get(entry[2])) * abs(entry[8])
+   total = stablechange[0] - startcount[0]
+   breakdown = list(stablechange[1:] - np.array(startcount[1:]))
+   return total, breakdown
 
 
 
-        
 def overnightstabling(daylist, u_list, change_matrix):
-    """ 
-    Finds the number of units back in each location at the end of the day
-    Uses the startofdayunitcount function as a startpoint, minimum required units for that day
-    Could be other, unused units which never left
-    """
-    
-    startcount = startofdayunitcount(daylist, u_list)
-    stablechange = np.array(startcount)
-    
-    for entry in daylist:
-        threecarscalar = 0.5 if entry[3] == 3 else 1
-
-        if entry[8] < 0:
-            stablechange -= np.array(change_matrix.get(entry[2]))*threecarscalar
-        else:
-            stablechange += np.array(change_matrix.get(entry[2]))*threecarscalar
-    
-    
-    if max(stablechange[0],startcount[0]) == stablechange[0]:
-        return stablechange[0],stablechange[1:]
-    else:
-        return startcount[0],startcount[1:]
+   """
+   Finds the number of units back in each location at the end of the day
+   Uses the startofdayunitcount function as a startpoint, minimum required units for that day
+   Could be other, unused units which never left
+   """
+   startcount = startofdayunitcount(daylist, u_list)
+   stablechange = np.array(startcount)
+   for entry in daylist:
+       if entry[8] < 0:
+           stablechange -= np.array(change_matrix.get(entry[2])) * abs(entry[8])
+       else:
+           stablechange += np.array(change_matrix.get(entry[2])) * abs(entry[8])
+   if max(stablechange[0], startcount[0]) == stablechange[0]:
+       return stablechange[0], stablechange[1:]
+   else:
+       return startcount[0], startcount[1:]
     
 
 
     
 def interpeakstabling(daylist, u_list):
-    """ 
-    Finds the maximum number of trains stabled at each location during interpeak
-    Returns the total and the unit breakdown at that point in time
-    """
-    
-    ip_tracker = []
-    prepeak = True
-    ip = startofdayunitcount(daylist, u_list)[0]
-    for t,x in enumerate(daylist):
-        
-        if len(x[7]) == 4:
-            x[7] = '0' + x[7]
-            
-        threecarscalar = 0.5 if x[3] == 3 else 1
-        ip += x[8] * threecarscalar
-        if '09:00:00' < x[7] < '15:30:00':
-            
-            while prepeak == True:
-                ip_tracker.append(
-                    (daylist[t-1][7], ip - daylist[t][8] * (0.5 if daylist[t][3] == 3 else 1))
-                )
-
-                
-                prepeak = False
-            
-            ip_tracker.append((x[7],ip))
-    
-    if ip_tracker:
-        traincount = [x[1] for x in ip_tracker]
-        output_total = max(traincount)
-        idx = traincount.index(output_total)
-        max_oclock = ip_tracker[idx][0]
-    else:
-        output_total = 0
-        
-    unit_subtotals = []
-    for u in u_list:
-        unit_ip = startofdayunitcount(daylist, u_list)[1:][u_list.index(u)]
-        for x in daylist:
-            try: 
-                max_oclock
-                
-                if x[2] == u:
-                    unit_ip += x[8] * (0.5 if x[3] == 3 else 1)
-                if x[7] == max_oclock:
-                    break
-            except:
-                break
-        if output_total == 0:
-            unit_ip = 0
-        unit_subtotals.append(unit_ip)    
-
-    return output_total,unit_subtotals
+   """
+   Finds the maximum number of trains stabled at each location during interpeak
+   Returns the total and the unit breakdown at that point in time
+   """
+   ip_tracker = []
+   prepeak = True
+   ip = startofdayunitcount(daylist, u_list)[0]
+   for t, x in enumerate(daylist):
+       if len(x[7]) == 4:
+           x[7] = '0' + x[7]
+       ip += x[8]
+       if '09:00:00' < x[7] < '15:30:00':
+           while prepeak == True:
+               ip_tracker.append(
+                   (daylist[t-1][7], ip - daylist[t][8])
+               )
+               prepeak = False
+           ip_tracker.append((x[7], ip))
+   if ip_tracker:
+       traincount = [x[1] for x in ip_tracker]
+       output_total = max(traincount)
+       idx = traincount.index(output_total)
+       max_oclock = ip_tracker[idx][0]
+   else:
+       output_total = 0
+   unit_subtotals = []
+   for u in u_list:
+       unit_ip = startofdayunitcount(daylist, u_list)[1:][u_list.index(u)]
+       for x in daylist:
+           try:
+               max_oclock
+               if x[2] == u:
+                   unit_ip += x[8]
+               if x[7] == max_oclock:
+                   break
+           except:
+               break
+       if output_total == 0:
+           unit_ip = 0
+       unit_subtotals.append(unit_ip)
+   return output_total, unit_subtotals

src/taipan/stabling/StablingCount.py
@@ -117,56 +117,41 @@ def TTS_SC(path, mypath = None):
         store = init_store(YARDS, SORT_ORDER_WEEK)
             
     
-        def write_day(sheet,daylist,row):
+        def write_day(sheet, daylist, row):
             """ Prints each run to the workbook and updates the unit count, printing the subsequent balance of all units """
-            
             startcount = startofdayunitcount(daylist, u_list)
             if daylist:
-                sheet.write_column( row,0,['Start of Day Unit Count','End of Day Unit Count'], size14)
-                sheet.write(        row,9, startcount[0],      tborder)
-                sheet.write_row(    row,10,startcount[1:],     border)
+                sheet.write_column(row, 0, ['Start of Day Unit Count', 'End of Day Unit Count'], size14)
+                sheet.write(row, 9, startcount[0], tborder)
+                sheet.write_row(row, 10, startcount[1:], border)
                 row += 2
                 stablechange = np.array(startcount)
-                for idx,entry in enumerate(daylist, row):
-                    unit = entry[2]
-                    cars = entry[3]
-
-                    threecarscalar = 0.5 if cars == 3 else 1
-    
+                for idx, entry in enumerate(daylist, row):
                     if entry[8] < 0:
-
-                        stablechange -= np.array(change_matrix.get(entry[2]))*threecarscalar
-                        
+                        stablechange -= np.array(change_matrix.get(entry[2])) * abs(entry[8])
                     else:
-                        stablechange += np.array(change_matrix.get(entry[2]))*threecarscalar
-                    
+                        stablechange += np.array(change_matrix.get(entry[2])) * abs(entry[8])
                     stablechange = list(stablechange)
-                    for j,cell in enumerate(entry+stablechange):
+                    for j, cell in enumerate(entry + stablechange):
                         sheet.write(idx, j, cell, formats[entry[2]]["normal"])
-                       
-                    # sheet.write_row(idx,0,entry+stablechange,font_dict.get(entry[2])[0])
                     if entry[5] in NON_STABLE_LOCATIONS:
                         sheet.write(idx, 5, entry[5], formats[entry[2]]["boldred"])
                     if entry[6] in NON_STABLE_LOCATIONS:
                         sheet.write(idx, 6, entry[6], formats[entry[2]]["boldred"])
-                        
-                sheet.write(        row-1,9,  stablechange[0],      tborder)
-                sheet.write_row(    row-1,10, stablechange[1:],     border)
-                
+                sheet.write(row - 1, 9, stablechange[0], tborder)
+                sheet.write_row(row - 1, 10, stablechange[1:], border)
                 row += len(daylist)
-                total      = stablechange[0]-startcount[0]
-                breakdown  = stablechange[1:]-np.array(startcount[1:])
-                unbalanced = any([total]+breakdown)
+                total = stablechange[0] - startcount[0]
+                breakdown = stablechange[1:] - np.array(startcount[1:])
+                unbalanced = any([total] + list(breakdown))
                 if unbalanced:
-                    sheet.write(        row,9,  total,      rborder)
-                    sheet.write_row(    row,10, breakdown,  rborder)
+                    sheet.write(row, 9, total, rborder)
+                    sheet.write_row(row, 10, breakdown, rborder)
                     sheet.set_tab_color('#CC194C')
                 else:
-                    sheet.write(        row,9,  total,      tborder)
-                    sheet.write_row(    row,10, breakdown,  border)
-                    
-                sheet.write(            row,0,  'Daily Difference', size14)
-                # startrow += len(daylist) + 5
+                    sheet.write(row, 9, total, tborder)
+                    sheet.write_row(row, 10, breakdown, border)
+                sheet.write(row, 0, 'Daily Difference', size14)
             
         def write_sheet(sheet,mon,tue,wed,thu,mth,fri,sat,sun):
             """ Populates the sheet with runs and totals for the whole week """